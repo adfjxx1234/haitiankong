a413f42cb5fa60fa59472a24e019aa37d0e9bff8
bogon <bogon@jinxiaoxu.local>
2015-03-09 18:54:17 +0800

    Initial Commit

---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by bogon on 15/3/9.
+//  Copyright (c) 2015年 sdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by bogon on 15/3/9.
+//  Copyright (c) 2015年 sdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
